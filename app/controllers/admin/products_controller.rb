fe0330636682655cf2b3ba97aa6b2afb1b100fa6
Eric Ho <endonoho@gmail.com>
2020-07-22 10:57:52 -0700

    Implement HTTP Basic Authentication for /admin/products page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV["BASIC_AUTH_USERNAME"], password: ENV["BASIC_AUTH_PASSWORD"]
+  #  http_basic_authenticate_with name: "dhh", password: "secret", except: :index
 
   def index
     @products = Product.order(id: :desc).all
