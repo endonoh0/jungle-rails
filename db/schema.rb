b555a61c600f0f31226b7ca1a56dcd15ad34db58
Eric Ho <endonoho@gmail.com>
2020-07-29 00:20:20 -0700

    Fix vistiors can visit product detail page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200728223847) do
+ActiveRecord::Schema.define(version: 20200729070333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,10 +69,12 @@ ActiveRecord::Schema.define(version: 20200728223847) do
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
   create_table "reviews", force: :cascade do |t|
-    t.integer "rating"
-    t.string  "description"
-    t.integer "user_id"
-    t.integer "product_id"
+    t.integer  "rating"
+    t.string   "description"
+    t.integer  "user_id"
+    t.integer  "product_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "reviews", ["product_id"], name: "index_reviews_on_product_id", using: :btree
