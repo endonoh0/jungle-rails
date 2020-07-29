c7c0c551a9364bfe9590137ae95911bc4bf168c1
Eric Ho <endonoho@gmail.com>
2020-07-28 22:26:12 -0700

    Update css on product details page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,5 @@ module ApplicationHelper
   def sold?(quantity)
     quantity === 0 ? true : nil
   end
+
 end
---
+++ app/helpers/reviews_helper.rb
@@ -0,0 +1,21 @@
+module ReviewsHelper
+
+  def getStars(rating)
+    case rating
+    when 1
+      'star1'
+    when 2
+      'star2'
+    when 3
+      'star3'
+    when 4
+      'star4'
+    else
+      'star5'
+    end
+  end
+
+  def verifyUser?(reviewer, user)
+    reviewer === user
+  end
+end
