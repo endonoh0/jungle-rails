9030bbaa5567532720bf739fabe5ce83b74b290c
Eric Ho <endonoho@gmail.com>
2020-07-29 16:53:37 -0700

    Implement inventory adjustment feature

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,19 @@ class Order < ActiveRecord::Base
 
   validates :stripe_charge_id, presence: true
 
+  after_create :adjust_product_quantity
+
+  private
+
+  def adjust_product_quantity
+    @order = Order.order("created_at").last
+    line_items = @order.line_items
+
+    line_items.each do |item|
+      product = Product.find(item.product_id)
+      product.quantity -= item.quantity
+      product.save
+    end
+  end
+
 end
